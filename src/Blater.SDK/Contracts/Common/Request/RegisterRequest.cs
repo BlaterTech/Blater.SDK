c1e341bef8c8ec0061d38d018cc33935406f4d60
breno <breno@autosystem.top>
2024-06-04 15:53:28 -0300

    ### Commit Message
    ```
    ✨ feat(Authentication): Implemented user registration endpoint
    
    - Added new user registration endpoint to AuthenticationEndpoint.cs.
    - Implemented validation for required fields (Email, Password, UserName).
    - Added logic to create and store a new user via IBlaterAuthStore.
    - Created RegisterRequest class in Blater.SDK for handling registration requests.
    - Updated BlaterUser model to make PhoneNumberConfirmed property private.
    
    Files affected:
    - src/Blater.Backend/Endpoints/AuthenticationEndpoint.cs
    - src/Blater/Models/BlaterUser.cs
    - src/Blater.SDK/Contracts/Common/Request/RegisterRequest.cs
    ```

---
+++ src/Blater.SDK/Contracts/Common/Request/RegisterRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Blater.SDK.Contracts.Common.Request;
+
+public class RegisterRequest
+{
+    public required BlaterId Id { get; set; }
+    public required string Email { get; set; }
+    public required string UserName { get; set; }
+    public required string Password { get; set; }
+}
