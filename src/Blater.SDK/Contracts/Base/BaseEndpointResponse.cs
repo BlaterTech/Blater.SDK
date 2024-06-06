c8adf31a41fd5a51945e6b13060dcc9c3b52cec2
breno <breno@autosystem.top>
2024-06-06 15:49:54 -0300

    ♻️ refactor: remove unnecessary usings and refactor multiple user endpoint classes
    - Removed unused namespaces from various files across the project.
    - Refactored `CreateUser`, `UpdateUser`, `DeleteUser`, and `FindOneUser` classes to improve structure and readability.
    - Introduced `GlobalUsings.cs` to centralize common usings for endpoints.
    - Simplified error handling and response sending in user endpoints.
    - Commented out unused endpoint classes and methods for potential future cleanup.
    ```
    
    ### Explanation
    
    - **GitMoji**: Used `♻️` (refactor) to indicate code changes that neither fix a bug nor add a feature.
    - **Description**:
      - **Removed unused namespaces**: Cleanup of unnecessary using statements to improve code clarity.
      - **Refactored endpoint classes**: Enhanced structure and readability of user endpoint classes.
      - **GlobalUsings.cs**: Centralized common usings

--- src/Blater.SDK/Contracts/Base/BaseEndpointResponse.cs
+++ src/Blater.SDK/Contracts/Base/BaseEndpointResponse.cs
@@ -3,9 +3,8 @@ using Blater.Resullts;
 
 namespace Blater.SDK.Contracts.Base;
 
-public class BaseEndpointResponse
+public class BaseEndpointResponse : BlaterResult
 {
     public BlaterCodes Status { get; set; }
     public string Message { get; set; } = null!;
-    public List<BlaterError>? Errors { get; set; }
 }
