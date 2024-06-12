333529d9c7e998973c51724dff8ef43d5e2be7bc
breno <breno@autosystem.top>
2024-06-12 10:39:47 -0300

    ✨ feat: Add changes stream functionality to Blater
    - Added `GetChanges` and `GetChangesQuery` methods to `CouchDatabaseClient`, `BlaterDatabaseStore`, `BlaterDatabaseRepository`, and related interfaces.
    - Implemented new endpoints for changes stream in the backend (`GetChanges` and `GetChangesQuery`).
    - Updated project references and folder structure in Blater.SDK and Blater.Backend.
    - Introduced `IBlaterAuthentication` interface in Blater.SDK.
    
    Changes include:
    - `src/Blater.Backend/Blater.Backend.csproj`
    - `src/Blater.Database/Clients/DatabaseClient/CouchDatabaseClient_Common.cs`
    - `src/Blater.Database/Implementations/Database/BlaterDatabaseStoreT.cs`
    - `src/Blater.Backend/Endpoints/v1/Database/Changes/GetChanges.cs`
    - `src/Blater.Database/Implementations/Database/BlaterDatabase

---
+++ src/Blater.SDK/Interfaces/IBlaterAuthentication.cs
@@ -0,0 +1,23 @@
+﻿using Blater.Resullts;
+using Blater.SDK.Contracts.Authentication.Request;
+using Blater.SDK.Contracts.Common.Request;
+
+namespace Blater.SDK.Interfaces;
+
+public interface IBlaterAuthentication
+{
+    Task<BlaterResult<bool>> ConfirmEmail(string email);
+    Task<BlaterResult<bool>> DeleteEmail(string email, DeleteBlaterUserRequest request);
+    
+    
+    Task<BlaterResult<string>> Login(AuthRequest request);
+    Task LoginProvider(string provider);
+    Task<BlaterResult<string>> SigninProvider(string provider);
+    
+    Task<BlaterResult<bool>> Register(RegisterBlaterUserRequest request);
+    
+    Task<BlaterResult<bool>> ResetEmail(string email, ResetBlaterUserEmailRequest request);
+    Task<BlaterResult<bool>> ResetPassword(string email, ResetBlaterUserPasswordRequest request);
+    
+    Task<BlaterResult<bool>> TwoFactor(string email, bool enabled);
+}
