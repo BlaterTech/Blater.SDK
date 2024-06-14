6744e75c0cb39e5b9d97b30b39c436273595fdb4
breno <breno@autosystem.top>
2024-06-13 21:22:53 -0300

    :recycle: ♻️ refactor: Remove generic constraints and update models and interfaces
    - Removed `BaseBlaterRole` and `BaseBlaterPermission` models.
    - Updated `BlaterRole` and `BlaterPermission` models to be more versatile.
    - Updated various interfaces to remove generic constraints and work with the new models.
    - Refactored endpoints to use the new models and interfaces.
    - Added helper methods and utilities to support new changes.
    - Updated tests to reflect changes in models and interfaces.
    ```
    
    ### Explanation
    
    This commit message follows the Conventional Commit convention and includes a GitMoji for better visual representation.
    
    1. **Type**: `:recycle: ♻️ refactor` indicates that this commit is a code change that neither fixes a bug nor adds a feature but refactors the existing codebase.
    2. **Scope**: The scope is implied to be related to models and interfaces within the `Bl

--- src/Blater.SDK/Contracts/UserRole/Request/Base/BaseUserRoleRequest.cs
+++ src/Blater.SDK/Contracts/UserRole/Request/Base/BaseUserRoleRequest.cs
@@ -5,5 +5,5 @@ namespace Blater.SDK.Contracts.UserRole.Request.Base;
 public abstract class BaseUserRoleRequest
 {
     public required BlaterUser BlaterUser { get; set; }
-    public required BaseBlaterRole BaseBlaterRole { get; set; }
+    public required string RoleName { get; set; }
 }
