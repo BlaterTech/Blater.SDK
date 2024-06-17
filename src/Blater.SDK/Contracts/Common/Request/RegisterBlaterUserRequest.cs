e2b7a36daf5f02d03b821742b02ffe05ec09e48f
breno <breno@autosystem.top>
2024-06-17 14:15:18 -0300

    ♻️ refactor: remove redundant exception tests and enhance user registration
    Eliminated redundant exception handling tests from `BlaterAuthStoreTests_Email.cs` and `BlaterAuthStoreTests_Login.cs` to streamline the test suite. Updated user-related endpoints including `DeleteUser`, `UpdateUser`, and `CreateUser` to implement role and permission checks. Improved the `BlaterHttpClient` to support better user registration and disposal. Added new user-related tests to ensure functionality.
    
    - Removed exception handling tests from `BlaterAuthStoreTests_Email.cs` and `BlaterAuthStoreTests_Login.cs`.
    - Enhanced `DeleteUser`, `UpdateUser`, and `CreateUser` endpoints to include role and permission requirements.
    - Implemented a new `Register` endpoint for user registration.
    - Added `Dispose` method to `BlaterHttpClient` for proper resource management.
    - Introduced comprehensive user tests in `UserTests.cs`.
    
    Files affected:
    -

--- src/Blater.SDK/Contracts/Common/Request/RegisterBlaterUserRequest.cs
+++ src/Blater.SDK/Contracts/Common/Request/RegisterBlaterUserRequest.cs
@@ -1,8 +1,7 @@
 ﻿namespace Blater.SDK.Contracts.Common.Request;
 
-public abstract class RegisterBlaterUserRequest
+public class RegisterBlaterUserRequest
 {
-    public required BlaterId Id { get; set; }
     public required string Email { get; set; }
     public required string UserName { get; set; }
     public required string Password { get; set; }
