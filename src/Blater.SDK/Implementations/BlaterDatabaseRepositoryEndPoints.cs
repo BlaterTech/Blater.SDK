8bbbbf4f8513f3b82d36b1371f6bfe78c6c165be
breno <breno@autosystem.top>
2024-06-20 10:24:46 -0300

    ✨ feat(db): enhance async query methods and error handling #42
    - Made `FindOne` and `FindMany` methods asynchronous.
    - Improved error handling using `HandleErrors`.
    - Added JSON deserialization for responses.
    
    These changes improve performance and reliability.

--- src/Blater.SDK/Implementations/BlaterDatabaseRepositoryEndPoints.cs
+++ src/Blater.SDK/Implementations/BlaterDatabaseRepositoryEndPoints.cs
@@ -1,45 +1,73 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Linq.Expressions;
+using Blater.Exceptions;
 using Blater.Interfaces;
+using Blater.JsonUtilities;
 using Blater.Query.Extensions;
 using Blater.Query.Interfaces;
 using Blater.Results;
 
 namespace Blater.SDK.Implementations;
 
-public class BlaterDatabaseRepositoryEndPoints<T>(BlaterDatabaseStoreEndPoints storeEndPoints) where T : BaseDataModel, IBlaterDatabaseRepository<T>
+public class BlaterDatabaseRepositoryEndPoints<T>(BlaterDatabaseStoreEndPoints storeEndPoints) 
+    where T : BaseDataModel, 
+    IBlaterDatabaseRepository<T>
 {
-    public Task<BlaterResult<T?>> FindOne(BlaterId id)
-    {
-        return storeEndPoints.Get<T?>($"{endpoint}/{id}");
-    }
-    
-    public Task<BlaterResult<T?>> FindOne(Expression<Func<T, bool>> predicate)
-    {
-        var query = predicate.ExpressionToBlaterQuery();
-        
-        if (query == null)
-        {
-            return Task.FromResult(new BlaterResult<T>(new BlaterError("Query Error")));
-        }
-        return storeEndPoints.Post<T?>($"{endpoint}", query);
-    }
-    
-    public Task<T?> FindOne(string partition, Expression<Func<T, bool>> predicate)
-    {
-        var query = predicate.ExpressionToBlaterQuery();
+    private readonly string _partition = typeof(T).FullName?.SanitizeString() ?? throw new BlaterException("Could not get the type name of the entity");
 
-        
-        if (query == null)
+    public async Task<BlaterResult<T?>> FindOne(BlaterId id)
+    {
+        var result = await storeEndPoints.Get(id);
+        if (result.HandleErrors(out var errors, out var response))
         {
-            return BlaterErrors.QueryError(predicate);
+            return errors;
         }
-        return storeEndPoints.Post<T?>($"{endpoint}", query);
+
+        var model = response.FromJson<T>();
+        return model;
     }
     
-    public Task<IReadOnlyList<T?>> FindMany(Expression<Func<T, bool>> predicate)
+    public async Task<BlaterResult<T?>> FindOne(Expression<Func<T, bool>> predicate)
     {
-        throw new NotImplementedException();
+        var query = predicate.ExpressionToBlaterQuery();
+        
+        var result = await storeEndPoints.QueryOne(_partition, query);
+        if (result.HandleErrors(out var errors, out var response))
+        {
+            return errors;
+        }
+        
+        var model = response.FromJson<T>();
+        return model;
+    }
+    
+    public async Task<BlaterResult<T?>> FindOne(string partition, Expression<Func<T, bool>> predicate)
+    {
+        var query = predicate.ExpressionToBlaterQuery();
+        
+        var result = await storeEndPoints.QueryOne(partition, query);
+        if (result.HandleErrors(out var errors, out var response))
+        {
+            return errors;
+        }
+        
+        var model = response.FromJson<T>();
+        return model;
+    }
+    
+    public async Task<BlaterResult<IReadOnlyList<T?>>> FindMany(Expression<Func<T, bool>> predicate)
+    {
+        var query = predicate.ExpressionToBlaterQuery();
+        
+        var result = await storeEndPoints.Query(_partition, query);
+        if (result.HandleErrors(out var errors, out var response))
+        {
+            return errors;
+        }
+
+        var models = response.Select(x => x.FromJson<T>()).ToReadOnlyList();
+
+        return new BlaterResult<IReadOnlyList<T?>>(models);
     }
     
     public Task<IReadOnlyList<T?>> FindMany(string partition, Expression<Func<T, bool>> predicate)
