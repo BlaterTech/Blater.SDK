68d63782c2be19905eae92c7af091af3d3d6eb55
vitor(PC Pedro) <vitor@autosystem.top>
2024-06-26 11:04:15 -0300

    Creating Auth Password store and repository

---
+++ src/Blater.SDK/Implementations/BlaterAuthentication/Repositories/BlaterAuthPasswordRepositoryEndPoints.cs
@@ -0,0 +1,19 @@
+﻿using Blater.Exceptions;
+using Blater.Interfaces.BlaterAuthentication.Stores;
+using Blater.Results;
+
+namespace Blater.SDK.Implementations.BlaterAuthentication.Repositories;
+
+public class BlaterAuthPasswordRepositoryEndPoints(BlaterAuthPasswordRepositoryEndPoints store) : IBlaterAuthPasswordStore
+{
+    public async Task<BlaterResult<bool>> ResetPassword(string email, string oldPassword, string newPassword)
+    {
+        var result = await store.ResetPassword(email, oldPassword, newPassword);
+        if (result.HandleErrors(out var errors, out var response))
+        {
+            throw new BlaterException(errors);
+        }
+
+        return response;
+    }
+}
---
+++ src/Blater.SDK/Implementations/BlaterAuthentication/Stores/BlaterAuthPasswordStoreEndPoints.cs
@@ -0,0 +1,20 @@
+﻿using Blater.Interfaces.BlaterAuthentication.Stores;
+using Blater.Results;
+using Blater.SDK.Contracts.Common.Request;
+
+namespace Blater.SDK.Implementations.BlaterAuthentication.Stores;
+
+public class BlaterAuthPasswordStoreEndPoints(BlaterHttpClient client) : IBlaterAuthPasswordStore
+{
+    private static string Endpoint => "/v1/Auth";
+    
+    public Task<BlaterResult<bool>> ResetPassword(string email, string oldPassword, string newPassword)
+    {
+        var request = new ResetBlaterUserPasswordRequest
+        {
+            NewPassword = newPassword,
+            OldPassword = oldPassword
+        };
+        return client.Post<bool>($"{Endpoint}/resetPassword/{email}", request);
+    }
+}
